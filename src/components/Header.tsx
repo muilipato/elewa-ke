6d4a701a46db69eae6428ca041e7686346c4866f
Patrick <muilipato@gmail.com>
2023-11-03 10:09:38 +0300

    🐛 bug: company logo  svg is passed as an img tag and it now renders

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,11 @@
+import React from 'react'
+import './styles/header.css'
 import companyLogo from '../assets/svgs/Logo_White_arvoyx.svg'
 export default function Header(){
     return(
         <header>
-            <div className="logo">{companyLogo}</div>
+            <div className="logo">
+                <img src={companyLogo} alt="company logo" /></div>
         </header>
     )
 }
