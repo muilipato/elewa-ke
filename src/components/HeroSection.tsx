65fd4a0762b2e9ef50c605fdffa5fd4197fad94a
Patrick <muilipato@gmail.com>
2023-11-03 13:59:13 +0300

    💄 hero-section style: Added correct fonts and positioning of the container

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import heroText from '../data/heroText.json'
+import './styles/heroSection.css'
 
 export default function HeroSection(){
     const {heading, description, description2}= heroText[0]
     return(
 
         <div className="hero-container">
-            <h1>{heading}</h1>
-            <p>{description}</p>
+            <p>{heading}</p>
+            <h1>{description}</h1>
             <p>{description2}</p>
         </div>
     )
