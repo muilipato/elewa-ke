c0b0983709055db76b46f4498f3313ccf25860d3
Patrick <muilipato@gmail.com>
2023-11-03 19:35:33 +0300

    ✨ Footer Component added to homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import Header from "./Header";
 import HeroSection from "./HeroSection";
+import Footer from "./Footer";
 import './styles/home.css'
 
 function Home(){
     return(
+      <>
         <div className="header-hero-content">
           <div className="background-image"></div>
           <Header/>
           <HeroSection/>
         </div>
+        <Footer/>
+      </>
     )
 }
 
