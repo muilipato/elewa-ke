b22771cc91e07121c6801d0b8f995a4d7d6ad1c8
Patrick <muilipato@gmail.com>
2023-11-03 09:50:04 +0300

    ✨ feat: created header starter code. Added logo only.

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+import companyLogo from '../assets/svgs/Logo_White_arvoyx.svg'
+export default function Header(){
+    return(
+        <header>
+            <div className="logo">{companyLogo}</div>
+        </header>
+    )
+}
