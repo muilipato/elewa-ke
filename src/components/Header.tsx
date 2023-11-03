e2f8a32e5c46ab884818fd57fff96d31eaec743f
Patrick <muilipato@gmail.com>
2023-11-03 11:06:09 +0300

    ✨ Import dependency:React-router-dom to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './styles/header.css'
+import menuItems from '../data/menuItems.json'
 import companyLogo from '../assets/svgs/Logo_White_arvoyx.svg'
 export default function Header(){
     return(
         <header>
             <div className="logo">
-                <img src={companyLogo} alt="company logo" /></div>
+                <img src={companyLogo} alt="company logo" />
+            </div>
+            <nav>
+                <ul className='menu'>
+                    {menuItems.map((item, index) => (
+                        <li key={index}>{item.text}</li>
+                    ))}
+                </ul>
+            </nav>
         </header>
     )
 }
